4328a8b1c2058db877b4f6c2ce63bd0774f3f6ef
murali0101 <muralimv0101@gmail.com>
2022-05-06 15:46:08 +0530

    Update ProductViewAll.jsx

--- src/components/Products/ProductViewAll.jsx
+++ src/components/Products/ProductViewAll.jsx
@@ -1,16 +1,64 @@
-import { useSelector } from "react-redux"
-
-
-
-
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getProduct, sortProduct } from "../../redux/product/action";
+import "./ProductViewAll.css";
 
 export const ProductViewAll = () => {
-    const ProductData = useSelector((store)=> {return store.product.productData})
-    console.log('ProductData:', ProductData)
-    return (<div className="all-product">
-        {ProductData.map((e) => {
-            return (<div key ={e.id}>{e.name }</div>)
-             })}
-             </div>)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getProduct("men"));
+  }, []);
+    function sortHandler(e) {
+   dispatch(sortProduct(e.target.value));
 }
+  const ProductData = useSelector((store) => {
+    return store.product.productData;
+  });
+  console.log("ProductData:", ProductData);
+    return (<div>
+        
+        <div className="sorting-prt">
+            <div><span className="bold-prt">SAVE SEARCH</span> View your saved searches</div>
+            <div>{ `${ProductData.length}+ items`}</div>
+            <div className="sort-prt">
+                <select onChange={sortHandler}>
+                    <option >SORT BY</option>
+                    <option value="high" >HIGHEST PRICE</option>
+                    <option value="low">LOWEST PRICE</option>
+                  
+                </select>
+            </div>
+</div>
+      <div className="all-product">
+          
+      {ProductData.map((e) => {
+        const { name, type, initialPrice, finalPrice, img, _id, size } = e;
+        function convert(a, b) {
+          const x = +a;
+          const y = +b;
+          return ((x - y) / x) * 100;
+        }
+        return (
+          <Link to={`/product/${_id}`} className="prt-div" key={_id}>
+            <img src={img}></img>
+            <div className="prt-name">{name}</div>
+            <div className="prt-type">{type}</div>
+            {initialPrice != "" ? (
+              <div className="prt-dis">
+                <div className="prt-initialPrice">{`$ ${initialPrice}`}</div>
+                <div className="prt-dis-price">{`${Math.floor(
+                  convert(initialPrice, finalPrice)
+                )} % OFF`}</div>
+              </div>
+            ) : (
+              <div className="ety"></div>
+            )}
+            <div className="prt-finalPrice">{` $ ${finalPrice}`}</div>
+          </Link>
+        );
+      })}
+        </div>
+        </div>
+  );
+};
