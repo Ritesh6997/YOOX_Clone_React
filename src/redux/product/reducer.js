4bf6b9fd110c1fbc20dd0e0f072eb0a5dff25b0f
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-05-06 23:44:25 +0530

    Delete src directory

--- src/redux/product/reducer.js
+++
@@ -1,9 +0,0 @@
-const init = {
-  productData: [],
-};
-export const productReducer = (store = init, { type, playload }) => {
-  switch (type) {
-    default:
-      return store;
-  }
-};
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
-import thunk from "redux-thunk";
-import { productReducer } from "./product/reducer";
-
-const rootReducer = combineReducers({
-  product: productReducer,
-});
-
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
