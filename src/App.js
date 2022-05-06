49e0158dba9d9f076cf3f5246d17a0c79bd51167
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2022-05-06 23:47:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import DesignComponent from "./components/DesignComponent"
+import ProductsDetails from "./components/productsDetails/ProductsDetails";
+
+function App() {
+
+  return (
+    <div className="App">
+      {/* <DesignComponent /> */}
+      <ProductsDetails  />
+    </div>
+  );
+}
+
+export default App;
