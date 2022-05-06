ab1fc9e22d037bbb272affdec5fa3ec399a6527c
prathyu Prasad <prathyuprasad04669@gmail.com>
2022-05-07 00:44:07 +0530

    design routing completed

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Products } from "./components/Products/Products";
 import { ProductViewAll } from "./components/Products/ProductViewAll";
 import { Routes, Route } from "react-router-dom";
 import ProtectedRoutes from "./components/Navbar/ProtectedRoutes";
+import DesignComponent from "./components/DesignComponent";
+import ProductsDetails from "./components/productsDetails/ProductsDetails";
 import Cart from "./components/Cart.jsx/Cart";
 function App() {
   return (
-      <div> 
-        {/* navBar */}
-        <Navbar></Navbar>
+    <div>
+      {/* navBar */}
+      <Navbar></Navbar>
       <Routes>
         {/* landing page -Route*/}
         {/* <Route path="/" element={<LandingPage />}></Route> */}
@@ -19,18 +21,20 @@ function App() {
         {/* women,men,kids page -Route*/}
         <Route path="/:name" element={<Products />}></Route>
         {/* design page -Route*/}
-        {/* <Route path="/design" element={<Design />}></Route> */}
+        <Route path="/design" element={<DesignComponent />}></Route>
         {/* ProductViewAll -Route*/}
         <Route path="/viewAll" element={<ProductViewAll />}></Route>
         {/* ProductDetails -Route*/}
-        {/* <Route path="/product/:id" element={<ProductDetails />}></Route> */}
+        <Route path="/product/:id" element={<ProductsDetails />}></Route>
         {/* Cart page -Route*/}
-        <Route path="/cart"
-            element={
-              <ProtectedRoutes>
-                <Cart />
-              </ProtectedRoutes>
-            }></Route>
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoutes>
+              <Cart />
+            </ProtectedRoutes>
+          }
+        ></Route>
         {/* checkout page -Route*/}
         {/* <Route path="/checkOut" element={<CheckOut />}></Route> */}
       </Routes>
---
+++ src/components/carousalComponent/ArrowMain.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { RiArrowRightSLine, RiArrowLeftSLine } from "react-icons/ri";
+
+export const SampleNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <RiArrowRightSLine
+      className={className}
+      style={{
+        ...style,
+        color: "grey",
+        width: "30px",
+        height: "46px",
+      
+      }}
+      onClick={onClick}
+    />
+  );
+};
+
+export const SamplePrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <RiArrowLeftSLine
+      className={className}
+      style={{
+        ...style,
+        color: "grey",
+        width: "30px",
+        height: "46px",
+     
+      }}
+      onClick={onClick}
+    />
+  );
+};
